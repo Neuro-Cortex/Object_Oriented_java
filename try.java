42d1c2200847100e8307f64843c14236f35a8a41
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-02 14:57:22 +0600

    Add files via upload

---
+++ try.java
@@ -0,0 +1,14 @@
+/*public class try {
+
+      class A{
+            int m=10;
+            void method1(int t){
+                  System.out.println(t);
+            }
+            static void method2(){
+                  static private final int m=
+            }
+      
+      }
+      
+}*/
