26b4604dfbb63d2893d0bdf683e8a62d7d4915cb
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-06 00:27:59 +0600

    Program 2

---
+++ gui1.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+public class gui1 {
+      public static void main(String[] args) {
+      
+       String name = JOptionPane.showInputDialog("Failla pollob:");
+       JOptionPane.showMessageDialog(null,name+"yes you are a busturd");
+           
+
+      }
+      
+}
