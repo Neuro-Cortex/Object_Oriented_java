4123ecfa20d8c1aa3a1e84644382a37a3d58d24b
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-06 00:28:51 +0600

    program 4

---
+++ gui2.java
@@ -0,0 +1,10 @@
+
+import javax.swing.JOptionPane;
+
+public class gui2 {
+public static void main(String[] args) {
+String l_name, f_name=JOptionPane.showInputDialog("what ever ","failla");
+l_name=JOptionPane.showInputDialog("what is your last name:");
+    JOptionPane.showMessageDialog(null, l_name, f_name, 0);
+}
+}
