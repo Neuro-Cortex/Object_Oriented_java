a60f024884ee97cabf6fc32c2fc1041725b80767
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-09 02:42:00 +0600

    h2

---
+++ ac.java
@@ -0,0 +1,26 @@
+/*import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.event.AncestorListener;
+
+public class ac extends JFrame implements AncestorListener {
+      JButton jb;
+      ac(){
+            jb=new JButton();
+            jb.setBounds(150,160,40,50);
+            this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setLayout(null);
+            this.setSize(50,50);
+            this.setVisible(true);
+            this.add(jb);
+      }
+      @Override
+      public void ActionPerformed(ActionEvent e){
+            if(e.gerSource()==button){
+                  System.out.print("Hello filla pollob what are you doing?");
+
+            }
+            
+      }
+      
+}
+*/
