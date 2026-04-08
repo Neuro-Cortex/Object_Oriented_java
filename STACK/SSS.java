7914e40fd3a8b1d961d0d26a079a88070dd12702
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-09 02:42:16 +0600

    f

---
+++ STACK/SSS.java
@@ -0,0 +1,27 @@
+/*package STACK;
+
+import java.util.Stack;
+
+public class SSS {
+      public static void main(String[] args) {
+            Stack <String> stk = new Stack<>();
+            stk.push("A");
+            stk.push("B");
+            stk.push("c");
+            System.out.print(stk+"\n");
+            String s = stk.pop();
+                       System.out.print(s+"\n");
+           stk.push("C,D");
+                           System.out.print(" "+stk);
+            String p = stk.peek();
+                           System.out.println(p);
+              System.out.println(stk.search("A"));
+
+
+
+
+
+
+      }
+      
+}*/
