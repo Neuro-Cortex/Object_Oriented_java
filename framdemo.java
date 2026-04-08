62734ec9f083f78560e1c6b81b725cf6be48aa0b
Pollob <104382051+Neuro-Cortex@users.noreply.github.com>
2026-04-08 19:51:02 +0600

    Add files via upload

---
+++ framdemo.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+
+public class framdemo {
+      public static void main(String[] args) {
+            JFrame j = new JFrame();
+            j.setVisible(true);
+          
+            j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            j.setSize(400, 600);
+            j.setLocationRelativeTo(null);
+
+      }
+      
+}
